ef1e1be10225d9602ab8247076579af362602d22
Gabriel Grabski <gabrielgrabski001@gmail.com>
2024-05-08 18:08:22 -0300

    initialize project

---
+++ src/test/java/com/grabski/authapi/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabski.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
